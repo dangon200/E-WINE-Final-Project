f782bb118b9a2109cd86f2eb3258712ae6cee6e3
Ignacio Cunial <igsecu@hotmail.com>
2022-10-11 17:33:42 -0300

    Action modifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
 import { Route } from "react-router-dom";
 
-import Choose from "./components/choose/Choose";
-import Login from "./components/login/Login";
-import Register from "./components/register/Register";
-import Dashboard from "./components/dashboard/Dashboard";
-
 function App() {
   return (
     <div className="App">
