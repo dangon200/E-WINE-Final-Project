a19d764608b3fad014e0cc100bb09c723112219c
ColoradaGreis <graciana.baratti@gmail.com>
2022-10-12 17:51:21 -0300

    client-App arreglo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage.jsx'
 
 function App () {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Route exact path='/' component={LandingPage} />
-      </div>
-    </BrowserRouter>
+    <div className='App'>
+      <Route exact path='/' component={LandingPage} />
+    </div>
   )
 }
 
