2d008453908912c2975490b7fbaaf885305d0de5
ColoradaGreis <graciana.baratti@gmail.com>
2022-10-13 16:13:18 -0300

    client- Navbar y arreglé el nombre del Searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage.jsx'
 import Error404 from './components/Error404/Error404.jsx'
+import About from './components/About/About.jsx'
+import Nav from './components/Nav/Nav'
 
 function App () {
   return (
     <div className='App'>
+      <Route path='/' component={Nav} />
       <Switch>
         <Route exact path='/' component={LandingPage} />
+        <Route exact path='/about' component={About} />
         <Route path='*' component={Error404} />
       </Switch>
     </div>
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,7 +1,30 @@
 // import style from './nav.module.css'
+import SearchBar from '../SearchBar/SearchBar.jsx'
+import { NavLink } from 'react-router-dom'
 
 export default function Nav () {
   return (
-    <div> Este es el Navbar</div>
+    <nav>
+      <div>
+        <SearchBar />
+      </div>
+      <div>
+        <NavLink to='/'>
+          <span>Inicio</span>
+        </NavLink>
+        <NavLink to='/about'>
+          <span>Sobre E-Wine</span>
+        </NavLink>
+        <NavLink to='/home'>
+          <span>Tienda</span>
+        </NavLink>
+        {/* <NavLink to='/Create'>
+          <span>Crear Cuenta</span>
+        </NavLink>
+        <NavLink>
+          <span>Iniciar Sesión</span>
+        </NavLink> */}
+      </div>
+    </nav>
   )
 }
