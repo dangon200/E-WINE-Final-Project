a8d03c970a357369f9a44818b3030f139c1de8a7
felipegc14 <felipegc000@gmail.com>
2022-10-13 09:28:16 -0300

    client-card-segundo

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,21 +1,23 @@
 import style from './card.module.css'
-import image from '../../utils/images/Bodegas-Akutain-Rioja-Gran-Reserva-2004.png'
 import { AiFillStar } from 'react-icons/ai'
 import { MdAddShoppingCart } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
-export default function Card () {
+export default function Card ({ id, name, image, score }) {
   return (
-    <div className={style.card}>
+    <div className={style.card} key={id}>
       <div className={style.divImage}>
         <img className={style.image} src={image} alt={image} />
       </div>
       <div className={style.dataCard}>
-        <h2 className={style.cardTitle}>Rioja Gran Reserva-2004</h2>
-        <h4><span><AiFillStar /></span> 4.7</h4>
+        <h2 className={style.cardTitle}>{name}</h2>
+        <h4><span><AiFillStar /></span>{score}</h4>
       </div>
       <div className={style.buttonCard}>
         <button className={style.cartButton}><span><MdAddShoppingCart /></span> Agregar</button>
-        <button className={style.detailButton}>Detalle</button>
+        <Link to='/detail'>
+          <button className={style.detailButton}>Detalle</button>
+        </Link>
       </div>
     </div>
   )
