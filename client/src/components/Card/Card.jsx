3a6ef724d7ac798dfbe96d24a1f34a46b457f2f2
felipegc14 <felipegc000@gmail.com>
2022-10-13 09:17:51 -0300

    client-card-primerosAvances

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,7 +1,22 @@
-import style from './Card.module.css'
+import style from './card.module.css'
+import image from '../../utils/images/Bodegas-Akutain-Rioja-Gran-Reserva-2004.png'
+import { AiFillStar } from 'react-icons/ai'
+import { MdAddShoppingCart } from 'react-icons/md'
 
-export default function Card ({ name, img, onClose }) {
+export default function Card () {
   return (
-    <div />
+    <div className={style.card}>
+      <div className={style.divImage}>
+        <img className={style.image} src={image} alt={image} />
+      </div>
+      <div className={style.dataCard}>
+        <h2 className={style.cardTitle}>Rioja Gran Reserva-2004</h2>
+        <h4><span><AiFillStar /></span> 4.7</h4>
+      </div>
+      <div className={style.buttonCard}>
+        <button className={style.cartButton}><span><MdAddShoppingCart /></span> Agregar</button>
+        <button className={style.detailButton}>Detalle</button>
+      </div>
+    </div>
   )
 }
