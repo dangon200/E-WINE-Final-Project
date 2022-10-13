a3df6f575c883b301af27dd5dafb2486a3b8a86b
Julian <julianmartinez_93@hotmail.com>
2022-10-13 18:03:55 -0300

    add redux and actions

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
-import { style } from './home.module.css'
+// import { style } from './home.module.css'
+import { useEffect } from 'react'
+import { getPublications, getProducts } from '../../store/actions/actions'
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function Home () {
+  const dispatch = useDispatch()
+  const products = useSelector(state => state.products)
+  const publications = useSelector(state => state.publications)
+  useEffect(() => {
+    dispatch(getProducts())
+    dispatch(getPublications())
+  }, [dispatch])
+  console.log(products)
+  console.log(publications)
   return (
-    <div />
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
--- client/src/store/actions/actions.js
+++ client/src/store/actions/actions.js
@@ -1,17 +1,83 @@
-export const REGISTER_USER = 'REGISTER USER'
+import axios from 'axios'
+const urlApi = 'https://e-wines-back.onrender.com'
 
-export function registerUser (data) {
-  return function (dispatch) {
-    fetch('http://localhost:3001/users/register', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-type': 'application/json'
-      }
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: REGISTER_USER, payload: data })
+export function getPublications () {
+  return async function (dispatch) {
+    try {
+      const api = await axios.get(`${urlApi}/publications`)
+      return dispatch({
+        type: 'GET_PUBLICATIONS',
+        payload: api.data
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export function getByPublication (id) {
+  return async function (dispatch) {
+    try {
+      const api = await axios.get(`${urlApi}/publications/${id}`)
+      return dispatch({
+        type: 'GET_BY_ID_PUBLICATION',
+        payload: api.data
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export function postPublication (data) {
+  return async function (dispatch) {
+    try {
+      const api = await axios.post(`${urlApi}/publications`, data)
+      return dispatch({
+        type: 'POST_PUBLICATION',
+        payload: api.data
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export function getProducts () {
+  return async function (dispatch) {
+    try {
+      const api = await axios.get(`${urlApi}/products`)
+      return dispatch({
+        type: 'GET_PRODUCTS',
+        payload: api.data
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export function getByIdProduct (id) {
+  return async function (dispatch) {
+    try {
+      const api = await axios.get(`${urlApi}/products/${id}`)
+      return dispatch({
+        type: 'GET_BY_ID_PRODUCT',
+        payload: api.data
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+// pending
+export const searchByNameProduct = (name) => {
+  return async function (dispatch) {
+    return fetch(`${urlApi}/products?name=${name}`)
+      .then((respuesta) => respuesta.json())
+      .then((infoProductName) => {
+        dispatch({ type: 'SEARCH_PRODUCT_BY_NAME', payload: infoProductName })
       })
   }
 }
