c16e16460eaf6dd1ac48d78f62aa1c06b207f92d
LHC <lautarohconti@gmail.com>
2022-10-14 21:35:33 -0300

    Last changes Juli

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import Card from '../Card/Card'
 import Pagination from '../pagination/Pagination'
 import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md'
-import SearchBar from '../SearchBar/SearchBar.jsx'
+// import SearchBar from '../SearchBar/SearchBar.jsx'
 
 export default function Home () {
   const dispatch = useDispatch()
