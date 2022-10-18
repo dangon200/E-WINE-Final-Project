3fe422f340ebe612122e8b7a2f40bd314ce240e9
Julian <julianmartine@hotmail.com>
2022-10-18 09:57:51 -0300

    responsive nav

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App () {
   })
 
   return (
-    <div className='App'>
+    <div className='App '>
       <Route exact path={['/', '/home', '/about', '/createPublication', '/publication/:id', '/carrito']} component={Nav} />
 
       <Switch>
