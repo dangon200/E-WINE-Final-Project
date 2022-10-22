eda2cea3c38b546f42fd4fe46b3b996ff542c7d6
Julian <julianmartinez_93@hotmail.com>
2022-10-22 17:50:57 -0300

    delete

--- client/src/components/FormLogin/FormLogin.jsx
+++ client/src/components/FormLogin/FormLogin.jsx
@@ -7,7 +7,6 @@ import jwtdecode from 'jwt-decode'
 import { schemaLogin } from '../utilities/schemas'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser, logoutUser, getFavorites } from '../../store/actions/actions'
-import $ from 'jquery'
 
 export default function FormLogin () {
   const cookies = new Cookies()
@@ -138,8 +137,6 @@ export default function FormLogin () {
     dispatch(logoutUser())
     cookies.remove('TOKEN')
   }
-  const modal = $('#exampleModal').on()
-  console.log('🚀 ~ file: FormLogin.jsx ~ line 142 ~ FormLogin ~ modal', modal)
   return (
     <div className='user-select-none'>
 
