3ad91ea90d8f9a5ac53bf579898ef3558a661be3
dangon200 <gonzalezdaniel230@gmail.com>
2022-10-23 18:26:56 -0300

    modificaciones webhooks

--- api/src/routes/webhooksMPRoutes.js
+++ api/src/routes/webhooksMPRoutes.js
@@ -1,15 +1,18 @@
 const server = require('express').Router()
 const { createBuy } = require('../controllers/webhooksMP')
 
-server.post('/', (req, res) => {
+server.post('/', (req, res, next) => {
   const id = req.body.data.id
-  try {
-    const newBuy = createBuy(id)
-    if (newBuy) {
-      res.status(200).send('Pago Realizado')
+  const payment = req.body.action
+  if (id !== undefined && payment === 'payment.created') {
+    try {
+      const newBuy = createBuy(id)
+      if (newBuy) {
+        res.status(200).send('Pago Realizado')
+      }
+    } catch (error) {
+      throw console.error(error)
     }
-  } catch (error) {
-    throw console.error(error)
-  }
+  } else next()
 })
 module.exports = server
